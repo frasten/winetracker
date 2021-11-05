216b8d09de97dc3f1bb5efba904604935d00fb12
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2021-11-05 22:23:33 +0100

    Further refactor: now the communication and data building is encapsulated into one function. This allows easy for loops.

--- run.py
+++ run.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 
-import requests
-from bs4 import BeautifulSoup
-from scraper_factory import buid_scraper
+from scraper import scrape
 
+urls = [
+    'https://www.tannico.it/morellino-di-scansano-riserva-docg-purosangue-2017-terenzi.html',
+    'https://www.tannico.it/chablis-aoc-vieilles-vignes-2018-domaine-raoul-gautherin-fils.html',
+    ]
 
-url = "https://www.tannico.it/morellino-di-scansano-riserva-docg-purosangue-2017-terenzi.html"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
-}
-
-scraper = buid_scraper(url)
-page = requests.get(url, headers=headers)
-soup = BeautifulSoup(page.content, "html.parser")
-
-price = scraper.ScrapePrice(soup)
-name = scraper.ScrapeName(soup)
-
-print(name)
-print(price)
+for url in urls:
+    data = scrape(url)
+    print(data.name, data.price)
---
+++ scraper.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+from scraper_factory import buid_scraper
+from datetime import datetime
+
+class ScrapeData:
+    url = None
+    price = 0.0
+    name = None
+    timestamp = datetime.now()
+
+
+def scrape(url: str) -> ScrapeData:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
+    }
+
+    scraper = buid_scraper(url)
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    price = scraper.ScrapePrice(soup)
+    name = scraper.ScrapeName(soup)
+
+    data = ScrapeData()
+    data.url = url
+    data.price = price
+    data.name = name
+    data.timestamp = datetime.now()
+
+    return data
