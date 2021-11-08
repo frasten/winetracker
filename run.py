ec4f0765e7e6f82363755423c0bbbfe838f3ffad
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2021-11-08 10:07:01 +0100

    Added Vino.com and Vinicum.com support

--- run.py
+++ run.py
@@ -5,6 +5,11 @@ from scraper import scrape
 urls = [
     'https://www.tannico.it/morellino-di-scansano-riserva-docg-purosangue-2017-terenzi.html',
     'https://www.tannico.it/chablis-aoc-vieilles-vignes-2018-domaine-raoul-gautherin-fils.html',
+    'https://www.vino.com/dettaglio/valpolicella-ripasso-superiore-doc-collezione-del-conte-villa-da-filicaja-2017.html',
+    'https://www.vino.com/dettaglio/franciacorta-rose-nature-docg-61-berlucchi-2014.html',
+    'https://www.vino.com/dettaglio/franciacorta-docg-saten-corte-alle-stelle.html',
+    'https://www.vinicum.com/i-vini/bundle-valdobbiadene-prosecco-86055-06.html',
+    'https://www.vinicum.com/i-vini/aiace-salice-salentino-riserva-doc-67374-06.html',
     ]
 
 for url in urls:
--- scraper_factory.py
+++ scraper_factory.py
@@ -22,12 +22,36 @@ class TannicoScraper(AbstractScraper):
         return tag.text
 
 
+class VinicumScraper(AbstractScraper):
+    def scrape_price(self, soup: BeautifulSoup) -> float:
+        tag = soup.find("span", class_="prodotto-main__price__final")
+        priceText = tag.text
+        return parse_italian_price(priceText)
+
+    def scrape_name(self, soup: BeautifulSoup) -> str:
+        tag = soup.find("h1", class_="prodotto-main__title")
+        return tag.text
+
+
+class VinoComScraper(AbstractScraper):
+    def scrape_price(self, soup: BeautifulSoup) -> float:
+        tag = soup.find("span", class_="prezzo")[0]
+        priceText = tag.text
+        return parse_italian_price(priceText)
+
+    def scrape_name(self, soup: BeautifulSoup) -> str:
+        tag = soup.find("div", id="product-bottle-info").find("h1")
+        return tag.text
+
+
 def buid_scraper(url: str) -> AbstractScraper:
     domain = urlparse(url).netloc
 
     try:
         return {
             "www.tannico.it": TannicoScraper(),
+            "www.vinicum.com": VinicumScraper(),
+            "www.vino.com": VinoComScraper(),
         }[domain]
     except:
         raise Exception(f"Website {domain} not yet supported!")
