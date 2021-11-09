d284f1024fbe7fbf548a4891a45935f5aebdc610
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2021-11-09 19:26:59 +0100

    Now item list is loaded from a text file.

--- run.py
+++ run.py
@@ -3,17 +3,15 @@
 from scraper import scrape
 import db
 
-urls = [
-    'https://www.tannico.it/morellino-di-scansano-riserva-docg-purosangue-2017-terenzi.html',
-    'https://www.tannico.it/chablis-aoc-vieilles-vignes-2018-domaine-raoul-gautherin-fils.html',
-    'https://www.vino.com/dettaglio/valpolicella-ripasso-superiore-doc-collezione-del-conte-villa-da-filicaja-2017.html',
-    'https://www.vino.com/dettaglio/franciacorta-rose-nature-docg-61-berlucchi-2014.html',
-    'https://www.vino.com/dettaglio/franciacorta-docg-saten-corte-alle-stelle.html',
-    'https://www.vinicum.com/i-vini/bundle-valdobbiadene-prosecco-86055-06.html',
-    'https://www.vinicum.com/i-vini/aiace-salice-salentino-riserva-doc-67374-06.html',
-    ]
-
-db.init()
+def load_items_list():
+    urls = []
+    with open('items.txt') as file:
+        for line in file:
+            url = line.rstrip()
+            if (len(url) == 0):
+                continue
+            urls.append(url)
+    return urls
 
 def insert_records_into_db(urls):
     items = []
@@ -25,6 +23,8 @@ def insert_records_into_db(urls):
     return items
 
 
+db.init()
+urls = load_items_list()
 items = insert_records_into_db(urls)
 
 for item in items:
